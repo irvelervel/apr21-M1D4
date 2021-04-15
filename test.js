08dd29861bf9271fbe3c698305c0cb50b93c0a39
Stefano Casasola <ste.casasola@gmail.com>
2021-04-15 09:42:31 +0200

    adds test js file

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log('hello strivers!!')
+console.log('another one')
+
+// STEPS FOR PUSHING LOCAL MODIFICATIONS INTO OUR ONLINE GITHUB REPOSITORY
+
+// 1) ADD THE FILES TO THE STAGING AREA (a place for temporarily store our modifications)
+// git add
+// 2) CREATE A COMMIT OUT OF THE FILES IN THE STAGING AREA
+// a commit is a BUNDLE of some modifications
+// git commit
+// 3) WE NEED TO PUSH OUR COMMIT ONLINE
+// git push
